29fe2df2cc9235aefeb273b46b7c0d15aa4f2478
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-03-05 13:59:45 -0300

    Alterando

--- 5- if-else.js
+++ 5- if-else.js
@@ -4,10 +4,10 @@ const idade = 15
 
 if (idade >= 18){
     console.log("Maior idade")
-} else if (idade >= 11){
+} else if (idade >= 15){
     console.log("Adolecente")
-} else if (idade >= 2) {
-    console.log("Bebê")
+} else if (idade >= 6) {
+    console.log("Criança")
 } else {
-    console.log("Pela amor de deus vuh")
+    console.log("Bebê")
 }
