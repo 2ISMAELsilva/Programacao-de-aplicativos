f84467ec492a162a728681b5efd4fa2cd8eabb0d
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-03-09 15:29:37 -0300

    Vetores

---
+++ 12- arrow function.js
@@ -0,0 +1,20 @@
+// Função 
+
+const somar = (a,b) => {
+    return a + b 
+}
+
+const soma = somar(2, 3)
+console.log(`Soma:  ${soma}`)
+
+
+
+// const subtrair =(a,b) => a- b 
+
+
+const subtra = (a,b) => {
+    return a + b 
+}
+
+const subtrair = subtra(2, 3)
+console.log(`Soma:  ${subtra}`)
