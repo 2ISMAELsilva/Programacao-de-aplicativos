99a37c3e818533b82210a798d36e61c8dc85b0a1
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-03-26 16:54:08 -0300

    React basico

---
+++ projeto/src/components/Mensagem/index.jsx
@@ -0,0 +1,13 @@
+import './style.css'
+
+function Mensagem() {
+    return(
+        // Comandos HTML
+        <div className='mensagem'>
+            <h1>Componente</h1>
+            <p>Este e um componente com uma Mensagem simples</p>
+        </div>
+    )
+}
+
+export default Mensagem
